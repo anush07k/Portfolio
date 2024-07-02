6d45e1d5aa87b31fcac6dbb14cf07756ea043a2a
ANUSHKA <anushd0708@gmail.com>
2024-07-02 21:36:35 +0530

    Demo commit

---
+++ Desktop/Portfolio/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Footer from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Footer/>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ Desktop/Portfolio/data/index.ts
@@ -0,0 +1,153 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Dedicated to Advancing Software Development and Optimizing Technical Processes. " ,
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast and problem solver with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a ",
+      description: "Chit-Chatra",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "NexusPay",
+      des: "A cutting-edge fintech banking platform built with Next.js, offering secure and seamless financial management.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/fm.svg"],
+      link: "https://github.com/anush07k/NexusPay",
+    },
+    {
+      id: 2,
+      title: "GenesisAI NFT",
+      des: "Used pre-developed AI model via Hugging Face to generate image, upload to IPFS and used blockchain technology to mint as NFT",
+      img: "/p2.svg",
+      iconLists: [ "/tail.svg", "re.svg", "/soli.svg", "/ipfs.png"],
+      link: "/ui.yoom.com",
+    },
+    {
+      id: 3,
+      title: "Dappazon",
+      des: "Decentralized marketing platform enabled with Metamask for a web 3.0 based shopping experience.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/soli.svg", "/ts.svg", "/metaa.png", "/tail.svg"],
+      link: "/ui.aiimg.com",
+    },
+    {
+      id: 4,
+      title: "Chit-Chatra",
+      des: "Advanced, next-generation chat solution leveraging the powerful MERN stack (MongoDB, Express.js, React, Node.js) alongside Socket.io for cutting-edge real-time communication. ",
+      img: "/p4.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/sock.svg", "/gsap.svg"],
+      link: "/ui.apple.com",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
